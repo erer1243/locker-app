732afa797c17dcfb8de08b53a06326197a1e7d2f
user <oli1243@live.com>
2017-12-29 14:11:50 -0500

    wip conversion to kvlang

--- failurepage.py
+++ failurepage.py
@@ -1,12 +1,33 @@
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
 import sys
+from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.app import App
+
 
 class FailureScreen(GridLayout):
-    def __init__(self, message, **kwargs):
+    message = ''
+    def __init__(self, reason, **kwargs):
         super().__init__(**kwargs)
-        self.cols = 1
-        self.rows = 2
-        self.add_widget(Label(text=message, size_hint=(1, .9), text_size=(self.width, None)))
-        self.add_widget(Button(text="Exit", on_release=sys.exit))
+        message = reason
+
+Builder.load_string('''
+<FailureScreen>:
+    rows: 2
+    cols: 1
+    Label:
+        text: root.getFailureMessage()
+    Button:
+        text: "exit"
+        on_release: sys.exit
+        size_hint_y: .1
+''')
+
+if __name__ == "__main__":
+    class FailureTest(App):
+        def __init__(self, message, **kwargs):
+            super().__init__(**kwargs)
+            self.grid = FailureScreen(message)
+        def build(self):
+            return self.grid
+
+    FailureTest("Generic error message. This appears only in debugging, you should never see this.").run()
