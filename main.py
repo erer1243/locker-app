6565d4f42aeb89f5bbb73119a2a51a35330e2851
user <oli1243@live.com>
2017-12-31 17:22:19 -0500

    tossed around what logic is where, most bt logic in app, display in manager

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # python stuff
 import sys
+from textwrap import wrap
+from time import sleep
 
 # java stuff
 import os
@@ -35,6 +37,31 @@ def error():
     ErrorMain(str(sys.exc_info())).run()
 
 class ScreenDisplayController(ScreenManager):
+    def addToBluetoothConnectionLog(self, message, dot=True):
+        log("ScreenDisplayController.addToBluetoothConnectionLog", message)
+        if dot:
+            to_be_appended = "•" + message
+        else:
+            to_be_appended = message
+        to_be_appended = '\n'.join(wrap(to_be_appended, 30))
+        to_be_appended += '\n'
+        self.ids.connection_progress_log.text += to_be_appended
+
+    def bluetoothBasedDisplayManager(self):
+        device_name = self.handleBluetoothID()              # get entered bluetooth ID if correct, else None
+        log("ScreenDisplayController.bluetoothBasedDisplayManager", "device name passed from handler: " + device_name)
+        if not device_name:                                 # if entered bluetooth ID is bad
+            return                                              # do nothing
+        from kivy.uix.screenmanager import NoTransition     # get NoTransition transition module to apply
+        self.transition = NoTransition()                    # set transition to nothing so it doesn't look odd
+        self.current = "makingBTconnection"                 # set screenmanager's screen to next one, move on completely
+        app = App.get_running_app()
+        self.addToBluetoothConnectionLog("Creating locker socket")
+        socket = app.createLockerSocket(device_name)
+        if not socket:
+            self.addToBluetoothConnectionLog("Could not create socket! Are you in range of the locker?", dot=False)
+
+
     header_red = False
     not_on_list_shown = False
     def handleBluetoothID(self):
@@ -50,34 +77,40 @@ class ScreenDisplayController(ScreenManager):
             log("ScreenDisplayController.handleBluetoothID", "Checking paired list for " + self.ids.idbox.text)
             if App.get_running_app().checkForLocker(self.ids.idbox.text):                                       # if that name is found
                 log("ScreenDisplayController.handleBluetoothID", "Bluetooth ID is on the paired list")              # log it
-                self.ids.header.color = (0, 1, 0, 1)                                                                # Set header color to green
+                self.ids.header.color = (1, 1, 1, 1)                                                                # Set header color to white
                 if self.not_on_list_shown:                                                                          # if the failure text is shown
                     self.ids.name_entry_grid.rows = 4                                                                   # remove it
-                        self.not_on_list_shown = False                                                                  # .
+                    self.not_on_list_shown = False                                                                      # .
                     self.ids.name_entry_grid.remove_widget(self.not_on_list)                                            # .
-                self.current = "scr1"                                                                               # set screenmanager's screen to next one
+                return self.ids.idbox.text
 
             else:                                                                                               # if that name is not found
-                import textwrap                                                                                 # get textwrap module
                 if not self.not_on_list_shown:                                                                  # if failure text isn't shown
                     self.not_on_list_shown = True                                                                   # record that it is now showing
                     from kivy.uix.label import Label                                                                # import label class
                     self.ids.name_entry_grid.rows = 5                                                               # add extra row to gridmanager
                     failtext = '\"' + self.ids.idbox.text + '\" is not on the paired list'                          # make failtext with entered bluetooth ID
-                    failtext = '\n'.join(textwrap.wrap(failtext, 25))                                               # wrap that failtext to fit screen
+                    failtext = '\n'.join(wrap(failtext, 25))                                               # wrap that failtext to fit screen
                     self.not_on_list = Label(text=failtext, color=(1,0,0,1), font_size=100)                         # create a label from that failtext
                     self.ids.name_entry_grid.add_widget(self.not_on_list)                                           # add the failtext to screen
 
                 else:                                                                                           # if the failuretext is already shown
                     failtext = '\"' + self.ids.idbox.text + '\" is not on the paired list'                      # make failtext with entered bluetooth ID
-                    failtext = '\n'.join(textwrap.wrap(failtext, 25))                                           # wrap the failtext to fit screen
+                    failtext = '\n'.join(wrap(failtext, 25))                                           # wrap the failtext to fit screen
                     self.not_on_list.text = failtext                                                            # change label to hold new failtext
                 log("ScreenDisplayController.handleBluetoothID", "Bluetooth ID is not on the paired list, displaying message")
-
+        return None
 
 class MainApp(App):
-    def log(self, tag, message):
-        log(tag, message)
+    UUIDString = "00001101-0000-1000-8000-00805F9B34FB"
+
+    def createLockerSocket(self, name):
+        for device in self.paired_devices:
+            if devices.getName() == name
+                return device.createRfcommSocketToServiceRecord(
+                    UUID.fromString(self.UUIDString)
+                )
+        return None
 
     def checkForLocker(self, name):
         for device in self.paired_devices:
@@ -90,9 +123,9 @@ class MainApp(App):
         if not self.bluetooth_adapter.isEnabled():
             log("MainApp.getBluetoothInfo", "Enabling bluetooth adapter")
             self.bluetooth_adapter.enable()
-            # wait for state to be STATE_ON
-            while(self.bluetooth_adapter.getState() != 12): # 12 is constant for STATE_ON
-                pass
+        # wait for state to be STATE_ON
+        while(self.bluetooth_adapter.getState() != 12): # 12 is constant for STATE_ON
+            pass
         # get paired devices from bluetoothadapter
         log("MainApp.getBluetoothInfo", "Getting paired devices")
         self.paired_devices = self.bluetooth_adapter.getBondedDevices().toArray()
