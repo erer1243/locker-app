7ecf68abf650f7070d0a0ff549a68620b8f44aeb
user <oli1243@live.com>
2018-01-01 20:06:09 -0500

    failure exit button > refresh button, huge leaps in bt connecting

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import sys
 from textwrap import wrap
 from time import sleep
 
+
 '''java stuff'''
 import os
 os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
 from jnius import autoclass
 # get android bluetooth classes
 BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
-BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
-BluetoothGattCallback = autoclass('android.bluetooth.BluetoothGattCallback')
-BluetoothGatt = autoclass('android.bluetooth.BluetoothGatt')
+# BluetoothGattCallback = autoclass('android.bluetooth.BluetoothGattCallback')
 UUID = autoclass('java.util.UUID')
-# log(String tag, String message) tag is an identifier, usually the class it's logging from
+# BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
+# BluetoothGatt = autoclass('android.bluetooth.BluetoothGatt')
+
 logd = autoclass('android.util.Log').d
 def log(tag, message):
     logd("\nlocker-controller." + tag, message+'\n')
+
 
 '''kivy stuff'''
 import kivy
@@ -27,7 +29,7 @@ from kivy.lang import Builder
 from kivy.clock import Clock
 # visual stuff
 from kivy.uix.popup import Popup
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
@@ -65,12 +67,18 @@ class ScreenDisplayController(ScreenManager):
         super().__init__(**kwargs)
         self.current = firstpage
 
+    def on_resume(self):
+        log("ScreenDisplayController.on_resume", "Device resuming, re-attaining paired bluetooth devices")
+        App.get_running_app().getBluetoothInfo()
+
     def bluetoothBasedDisplayManager(self, ID):
         name_good = self.handleBluetoothID(ID) # get entered bluetooth ID if correct, else None
         log("ScreenDisplayController.bluetoothBasedDisplayManager", "device name passed from handler: " + ID)
         if not name_good:                    # if entered bluetooth ID is bad
             return                                 # do nothing
         app = App.get_running_app()
+        log("ScreenDisplayController.bluetoothBasedDisplayManager", "Trying to connect to device.")
+        log("ScreenDisplayController.bluetoothBasedDisplayManager", str(app.connectToDevice()))
 
     def handleBluetoothID(self, ID):
         app = App.get_running_app()
@@ -95,16 +103,41 @@ class MainApp(App):
     # get bluetooth default adapter
     # assumes device can use bluetooth!
     bluetooth_adapter = BluetoothAdapter.getDefaultAdapter()
-    bluetooth_gatt_callback = BluetoothGattCallback()
+    # bluetooth_gatt_callback = BluetoothGattCallback()
 
     # UUIDString = "00001101-0000-1000-8000-00805F9B34FB" # generic primary access, not adafruit specific
-    # UUIDString = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" # generic uart from adafruit site
-    UUIDString = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" # TX uart from adafruit site
-    # UUIDString = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" # RX uart from adafruit site
+    uart_service_uuid = UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E") # generic uart from adafruit site
+    tx_uuid = UUID.fromString("6E400002-B5A3-F393-E0A9-E50E24DCCA9E") # TX uart from adafruit site
+    rx_uuid = UUID.fromString("6E400003-B5A3-F393-E0A9-E50E24DCCA9E") # RX uart from adafruit site
+
+    def connectToDevice(self):
+        def fail():
+            popup("Bluetooth Connection Error", "Could not connect to \'" + self.device.getName() + "\', make sure you are close enough to it and it is powered on.")
+        # we're working without a callback class here
+        # have to just time stuff and check if there's a connection
+        log("MainApp.connectToDevice", "Trying to connect to device")
+        gatt = self.device.connectGatt(None, True, None)
+        log("MainApp.connectToDevice", "Attempting to get uart service")
+        connected = False
+        for _ in range(0, 10):
+            sleep(1)
+            uart_service = gatt.getService(self.uart_service_uuid)
+            if uart_service:
+                log("MainApp.connectToDevice", "UART service found!")
+                connected = True
+                break
+        if not connected:
+            log("MainApp.connectToDevice", "UART service NOT found!")
+            fail()
+            return False
+
+        return True
 
     def checkForLocker(self, name):
         for device in self.paired_devices:
             if device.getName() == name:
+                self.device = device
+                log("MainApp.checkForLocker", "Device to be used set to " + str(device))
                 return True
         return False
 
@@ -117,7 +150,7 @@ class MainApp(App):
         while(self.bluetooth_adapter.getState() != 12): # 12 is constant for STATE_ON
             pass
 
-    def getBluetoothInfo(self):
+    def initBluetoothInfo(self):
         self.startBluetoothAdapter()
         # get paired devices from bluetoothadapter
         log("MainApp.getBluetoothInfo", "Getting paired devices")
@@ -129,15 +162,33 @@ class MainApp(App):
         log("MainApp.getBluetoothInfo", "Phone has paired devices")
         return True
 
+    def getBluetoothInfo(self):
+        def correctScreen(name):
+            if not self.SDC.current == name:
+                self.SDC.current = name
+        self.startBluetoothAdapter()
+        # get paired devices from bluetoothadapter
+        log("MainApp.getBluetoothInfo", "Getting paired devices")
+        self.paired_devices = self.bluetooth_adapter.getBondedDevices().toArray()
+        # if paired devices is empty
+        if not self.paired_devices:
+            correctScreen("no_paired_devices_failure")
+            log("MainApp.getBluetoothInfo", "No paired devices found, failing!")
+            return False
+        correctScreen("name_entry")
+        log("MainApp.getBluetoothInfo", "Phone has paired devices")
+        return True
+
     def build(self):
         Builder.load_file('main.kv')
 
-        if not self.getBluetoothInfo():
+        if not self.initBluetoothInfo():
             log("MainApp.build", "Loading first screen no_paired_devices_failure")
             self.SDC = ScreenDisplayController("no_paired_devices_failure")
         else:
             log("MainApp.build", "Loading first screen name_entry")
             self.SDC = ScreenDisplayController("name_entry")
+        self.SDC.transition = NoTransition()
         return self.SDC
 
 class AppManager():
