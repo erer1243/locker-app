bbc031759e917b59329f7c37eb8479582733789e
erer1243 <oli1243@live.com>
2018-01-02 10:29:09 -0500

    testing callback class

--- BluetoothGattCallback.java
+++ BluetoothGattCallback.java
@@ -1,9 +1,35 @@
 package lockerapp;
 
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.util.Log;
 import static java.lang.System.*;
 
-public class BluetoothGattCallback {
-  public static void test(){
-    out.println("Hello!!");
+class CustomGattCallback extends BluetoothGattCallback {
+  private static final int STATE_DISCONNECTED = 0;
+  private static final int STATE_CONNECTED = 1;
+  private int connection_state;
+
+  public CustomGattCallback(){
+    log("CustomGattCallback", "Initialized CustomGattCallback");
+    connection_state = STATE_DISCONNECTED;
+    log("CustomGattCallback", "connection state set to 0, STATE_DISCONNECTED.");
+  }
+
+  @Override
+  public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState){
+    log("onStateChange", "Status: " + str(status) + " newState: " + str(newState));
+  }
+
+  // helper methods/macros here out
+  public void log(String tag, String message){
+    Log.d("locker-controller.BluetoothGattCallback" + tag, message);
+  }
+
+  public String str(int input){
+    return Integer.toString(input);
   }
 }
