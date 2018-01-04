ba4bfb0a4a9f685dbf5cd0e627de0fc5f9587797
user <oli1243@live.com>
2018-01-03 23:08:48 -0500

    Moved code around to make it a little more readable

--- BluetoothGattCallback.java
+++ BluetoothGattCallback.java
@@ -5,17 +5,19 @@ import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothProfile;
 import android.util.Log;
 import java.util.List;
 import java.util.UUID;
 import static java.lang.System.*;
 
 class BTManager extends BluetoothGattCallback {
-  private static final int STATE_DISCONNECTED = BluetoothProfile.STATE_DISCONNECTED;
-  private static final int STATE_CONNECTED = BluetoothProfile.STATE_CONNECTED;
+  private static final int STATE_DISCONNECTED = 0;
+  private static final int STATE_CONNECTING = 1;
+  private static final int STATE_CONNECTED = 2;
+  private static final int STATE_DISCONNECTING = 3;
 
-  private BluetoothGatt bt_gatt;
+  private BluetoothGattCharacteristic tx_char;
+  private BluetoothGattCharacteristic rx_char;
   private int connection_state;
   private boolean ever_connected = false;
   private UUID tx;
@@ -25,7 +27,6 @@ class BTManager extends BluetoothGattCallback {
   public BTManager(UUID uart_uuid, UUID tx_uuid, UUID rx_uuid){
     log("CustomGattCallback", "Initialized CustomGattCallback");
     connection_state = STATE_DISCONNECTED;
-    log("CustomGattCallback", "connection state set to 0, STATE_DISCONNECTED.");
     uart = uart_uuid;
     tx = tx_uuid;
     rx = rx_uuid;
@@ -34,47 +35,50 @@ class BTManager extends BluetoothGattCallback {
   @Override
   public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState){
     log("onStateChange", "Status: " + Integer.toString(status) + " newState: " + Integer.toString(newState));
-
-    if(newState == STATE_CONNECTED){
-      log("onStateChange", "Bluetooth device initially connected!");
-      log("onStateChange", "Discovering services");
-      gatt.discoverServices();
-      ever_connected = true;
-      connection_state = STATE_CONNECTED;
-    }
-
-    if(newState == STATE_DISCONNECTED){
-      log("onStateChange", "Bluetooth device disconnected!");
-      connection_state = STATE_DISCONNECTED;
+    switch(newState){
+      case STATE_CONNECTED:
+        onConnect(gatt);
+        break;
+      case STATE_DISCONNECTED:
+        onDisconnect();
+        break;
+      default: break;
     }
   }
 
   public void onServicesDiscovered(BluetoothGatt gatt, int status){
     log("onServicesDiscovered", "Service discovered. Status: " + Integer.toString(status));
+
     if(status == 0){
       log("onServicesDiscovered", "All services discovered, checking for uart service");
 
       if(gatt.getService(uart) != null){
-        log("onServicesDiscovered", "UART FOUND OOO");
+        log("onServicesDiscovered", "Uart service found");
         BluetoothGattService uart_service = gatt.getService(uart);
+
         if(uart_service.getCharacteristic(tx) != null){
+          log("onServicesDiscovered", "TX characteristic found. This is a compatible device.");
+          tx_char = uart_service.getCharacteristic(tx);
+          rx_char = uart_service.getCharacteristic(rx);
 
-          log("onServicesDiscovered", "TX characteristic found! This is a proper device.");
-          BluetoothGattCharacteristic tx_char = uart_service.getCharacteristic(tx);
-          BluetoothGattCharacteristic rx_char = uart_service.getCharacteristic(rx);
-
-          tx_char.setValue("TEST!");
+          tx_char.setValue("tEsT");
           gatt.writeCharacteristic(tx_char);
-          log("onServicesDiscovered", "Test sent! Look for it on serial line");
+          log("onServicesDiscovered", "Test sent. Look for it on serial line.");
         }
-        else{
-          log("onServicesDiscovered", "tx service not found :^(");
-        }
-      }
-      else{
-        log("onServicesDiscovered", "Uart not found :^(");
       }
     }
+  }
+
+  public void onConnect(BluetoothGatt gatt){
+    log("onStateChange", "Bluetooth connected, discovering services.");
+    gatt.discoverServices();
+    ever_connected = true;
+    connection_state = STATE_CONNECTED;
+  }
+
+  public void onDisconnect(){
+    log("onStateChange", "Bluetooth device disconnected.");
+    connection_state = STATE_DISCONNECTED;
   }
 
   public int getConnectionState(){
